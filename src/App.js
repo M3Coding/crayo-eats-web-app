73845cfc8916f7605918c633695f5dacd885b4dd
M3Coding <meady2009@gmail.com>
2026-01-03 20:12:31 -0500

    html strcuture complete- About route add to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Restaurants from "./pages/Restaurant";
-
+import About from "./pages/About";
 import MenuPage from "./components/MenuPage.jsx"
 import RestaurantPageHeader from "./components/RestaurantPageHeader.jsx";
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
         <Route path="/Restaurants" element={<Restaurants />} />
         <Route path="/MenuPage/:id/" element={<MenuPage />}></Route>
       </Routes>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,37 @@
 function About () {
     return (
-        <div>
+        <div className="about-container">
             <div>
-                <img />
-                <p></p>
+                <div className="about-header">
+                    <h1>Our Vibe</h1>
+                </div>
+                <div className="header-image-container">
+                    <h2>Crayo Eats LLC</h2>
+                    <img src="/images/profile-placeholder.jpg" />
+
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div>
+                <div className="about-header">
+                    <h1>Our Founder/CEO</h1>
+                </div>
+                <div className="header-image-container">
+                    <h2>Chris Carswell</h2>
+                    <img src="/images/profile-placeholder.jpg" />
+
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                
+                
+                
             </div>
         </div>
     )
 }
+
+export default About;
